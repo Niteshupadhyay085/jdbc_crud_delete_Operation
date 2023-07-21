5abfa06e9cce9d67bafc0745bb6d2f0d817c29f6
Nitesh ratan upadhyay <140079012+Niteshupadhyay085@users.noreply.github.com>
2023-07-20 17:57:25 -1200

    Add files via upload

---
+++ DeleteStudentController.java
@@ -0,0 +1,16 @@
+package com.jspider.jdbc_simple_crud_operation.controller;
+
+public class DeleteStudentController {
+	public static void main(String[] args) {
+		
+		
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+}
